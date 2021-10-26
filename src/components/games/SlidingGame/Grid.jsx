5fe5bc13b13f9275870052d068eac5f906d4ce8b
soul0101 <53980340+soul0101@users.noreply.github.com>
2021-10-26 17:18:02 +0530

    Fixed sliding game

--- src/components/games/SlidingGame/Grid.jsx
+++ src/components/games/SlidingGame/Grid.jsx
@@ -15,80 +15,6 @@ var images = [
   "//i.giphy.com/l0NwLUVdksjwmtgLC.gif",
 ];
 
-// var Grid = React.createClass({
-//   getInitialState: function () {
-//     return {
-//       imageLoaded: false,
-//       imageObject: null,
-//     };
-//   },
-
-//   componentDidMount: function () {
-//     this.loadNewImage();
-//   },
-
-//   loadNewImage: function () {
-//     var image = new Image();
-//     image.src = images[Math.floor(Math.random() * images.length)];
-//     this.setState({
-//       imageLoaded: image.complete,
-//       imageObject: image,
-//     });
-//     var grid = this;
-//     image.onload = function () {
-//       grid.state.imageLoaded = true;
-//       grid.setState(grid.state);
-//     };
-//   },
-
-//   tileClick: function (element, position) {
-//     this.props.moveTile(position);
-//   },
-
-//   render: function () {
-//     if (this.state.imageObject && this.state.imageLoaded) {
-//       if (this.props.model.isSolved()) {
-//         return (
-//           <div id="grid">
-//             <img src={this.state.imageObject.src} width="450" />
-//           </div>
-//         );
-//       } else {
-//         var tileWidth = this.state.imageObject.width / this.props.model.size;
-//         var tileHeight = this.state.imageObject.height / this.props.model.size;
-
-//         return (
-//           <div id="grid">
-//             <FlipMove duration="100">
-//               {this.props.model.tiles.map(function (number, currentPosition) {
-//                 var drawImage = number ? true : false;
-//                 var expectedPosition = number - 1;
-//                 var coordinates = this.props.model.getTileCoordinates(expectedPosition);
-//                 return (
-//                   <Tile
-//                     key={number}
-//                     position={currentPosition}
-//                     coordinates={coordinates}
-//                     drawImage={drawImage}
-//                     img={this.state.imageObject}
-//                     width={tileWidth}
-//                     height={tileHeight}
-//                     click={this.tileClick}
-//                   />
-//                 );
-//               }, this)}
-//             </FlipMove>
-//           </div>
-//         );
-//       }
-//     } else {
-//       return <div id="grid">Loading...</div>;
-//     }
-//   },
-// });
-
-// module.exports.default = Grid;
-
 const Grid = (props) => {
   useEffect(() => {
     loadNewImage();
@@ -107,7 +33,7 @@ const Grid = (props) => {
     };
   };
 
-  const titleClick = (position) => {
+  const tileClick = (element, position) => {
     props.moveTile(position);
   };
 
@@ -115,7 +41,7 @@ const Grid = (props) => {
     if (props.model.isSolved()) {
       return (
         <div id="grid">
-          <img src={imageObject.src} width="450" />
+          <img src={imageObject.src} width={props.gameSize} alt="Completed Puzzle" />
         </div>
       );
     } else {
@@ -138,7 +64,7 @@ const Grid = (props) => {
                   img={imageObject}
                   width={tileWidth}
                   height={tileHeight}
-                  click={titleClick}
+                  click={tileClick}
                 />
               );
             })}
