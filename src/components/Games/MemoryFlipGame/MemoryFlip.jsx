77908707a58dfbcfb6adcd66b0161ead3412be61
Ayush Yadav <ayushyadav935@gmail.com>
2021-11-06 00:01:43 +0530

    Memory Flip Game done

--- src/components/Games/MemoryFlipGame/MemoryFlip.jsx
+++ src/components/Games/MemoryFlipGame/MemoryFlip.jsx
@@ -5,20 +5,15 @@ import Board from "./board";
 import Summary from "./summary";
 import { withRouter } from "react-router-dom";
 import ScoreBoard from "./board/ScoreBoard";
-import { useDispatch } from "react-redux";
-import { removeHint } from "../../../redux/actions/hintActions";
 
 MemoryFlip.defaultProps = {
   gridSize: [5, 2],
 };
 
-function MemoryFlip({ gridSize, products,seconds,setSeconds }) {
-  // const GRID_SIZES = [
-  //   [3, 4],
-  //   [5, 2],
-  //   [4, 4],
-  //   [4, 5],
-  // ];
+function MemoryFlip({ gridSize, products,seconds,setSeconds,setgameOver }) {
+  const [score, setscore] = useState(null)
+  const [timeTaken, settimeTaken] = useState(null)
+  const [currentScore, setcurrentScore] = useState(0)
 
   const GAME_VIEWS = {
     LOBBY: "lobby",
@@ -27,11 +22,6 @@ function MemoryFlip({ gridSize, products,seconds,setSeconds }) {
   };
 
   const [viewOption, setViewOption] = useState(GAME_VIEWS.PLAYING);
-  const dispatch = useDispatch();
-  // const [gridSize, setGridSize] = useState(GRID_SIZES[1]);
-
-  //console.log("here : ",viewOption);
-
   const [gameStats, setGameStats] = useState({
     matched: null,
     matchedAll: null,
@@ -39,18 +29,42 @@ function MemoryFlip({ gridSize, products,seconds,setSeconds }) {
   });
 
   useEffect(() => {
-    //console.log(gameStats);
+    if(gameStats.moves!==0 && seconds!==0){
+      setcurrentScore(Math.round(10000/(seconds*gameStats.moves)))
+    }
+  }, [gameStats.moves,seconds])
+
+  useEffect(() => {
     if(viewOption === GAME_VIEWS.SUMMARY){
+      setgameOver(true);
+      settimeTaken(seconds)
       console.log("Game Completed : ",gameStats.moves);
       console.log("Time Taken : ",seconds);
-
+      fetch("https://myntrah-backend.herokuapp.com/sendScore", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem("token")}`,
+          },
+          body: JSON.stringify({gametype :"memory-flip",moves: gameStats.moves,time: seconds})
+        })
+          .then((res) => res.json())
+          .then(
+            (result) => {
+              console.log(result)
+              if (result.success) {
+                setscore(result.data.score)
+              } else {
+                console.log(result.message)
+              }
+            },
+            (error) => {
+              console.log(error)
+            }
+          );
+          setSeconds(0)
       localStorage.removeItem('hint')
       localStorage.removeItem('time')
-      //Gugad
-      setSeconds(0);
-      setTimeout(() => {
-        dispatch(removeHint());
-      }, 10000);
     }
     // eslint-disable-next-line
   }, [viewOption]);
@@ -76,35 +90,14 @@ function MemoryFlip({ gridSize, products,seconds,setSeconds }) {
 
       {viewOption === GAME_VIEWS.SUMMARY && (        
         <Summary
+          score={score}
+          timeTaken={timeTaken}
+          currentScore={currentScore}
           GAME_VIEWS={GAME_VIEWS}
           setViewOption={setViewOption}
           gameStats={gameStats}
         />
       )}
-
-      {/* <Route
-        exact
-        path="/memory/"
-        render={() => {
-          setViewOption(GAME_VIEWS.LOBBY);
-          return renderGame(GAME_VIEWS.LOBBY);
-        }}
-      />
-      <Route
-        path="/memory/lobby"
-        render={() => {
-          setViewOption(GAME_VIEWS.LOBBY);
-          return renderGame(GAME_VIEWS.LOBBY);
-        }}
-      />
-      <Route
-        path="/memory/playing"
-        render={() => {
-          setViewOption(GAME_VIEWS.PLAYING);
-          return renderGame(GAME_VIEWS.PLAYING);
-        }}
-      /> */}
-      {/* <Route path="/memory/summary" render={() => renderGame(GAME_VIEWS.SUMMARY)} /> */}
     </div>
   );
 }
--- src/components/Games/MemoryFlipGame/summary/index.jsx
+++ src/components/Games/MemoryFlipGame/summary/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Redirect, withRouter } from "react-router-dom";
 
-function Summary({ gameStats }) {
+function formatTime(s) {
+  let min = ((s / 60) | 0).toString().padStart(2, "0");
+  let sec = (s % 60).toString().padStart(2, "0");
+  return min + ":" + sec;
+}
+
+function Summary({ gameStats,score,currentScore,timeTaken }) {
   if (!gameStats.matched) {
     return <Redirect to="/lobby" />;
   }
@@ -15,6 +21,12 @@ function Summary({ gameStats }) {
           You matched all {gameStats.matched.length} cards,
           <br />
           Good memory!
+          <br/>
+         Your Current Score {currentScore} 
+          <br/> 
+          {score? <span>{`Your Total score: ${Math.round(score)}`}</span>:""}
+          <br/> 
+          {timeTaken && <span>Time Taken: {formatTime(timeTaken)}</span>}
         </div>
       </div>
     </h2>
