5d4d37879d17662b84a6ec9c081217060d9f67e4
Ayush Yadav <ayushyadav935@gmail.com>
2021-11-07 11:02:20 +0530

    Minors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginScreen from "./components/User/Login/Login";
-// import ProductDetails from "./components/Products/ProductDetails";
 import Home from "./components/Home";
 import Category from "./components/Products/Category";
 require("dotenv").config();
@@ -12,7 +11,6 @@ function App() {
       <Router>
         <Switch>
           <Route path="/" exact component={Home} />
-          {/* <Route path="/product/:productId" component={ProductDetails} /> */}
           <Route path="/category/:category" component={Category} />
           <Route path="/gender/:gender" component={Category} />
           <Route path="/login" component={LoginScreen} />
